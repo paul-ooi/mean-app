882428096c258fe708488bb8bf7d5c3ff2136409
Geri Kona <gerikona@hotmail.com>
2018-06-05 09:29:17 -0400

    Functionality broken down into components

---
+++ src/app/comms.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommsService {
+
+  private search = new BehaviorSubject<string>('');
+  searchFilter = this.search.asObservable();
+
+  constructor() {
+  }
+
+  changeSearch(selection: string) {
+    this.search.next(selection);
+  }
+}
---
+++ src/app/places/places.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {Place} from '../place';
+import {PlaceService} from '../place.service';
+import {CommsService} from '../comms.service';
+
+@Component({
+  selector: 'app-places',
+  templateUrl: './places.component.html',
+  styleUrls: ['./places.component.css']
+})
+export class PlacesComponent implements OnInit {
+  searchFilter: string;
+
+  places: Place[] = [];
+
+  constructor(private placeService: PlaceService, private comms: CommsService) { }
+
+  ngOnInit() {
+    this.placeService.getPlaces().subscribe(data => this.places = data)
+    this.comms.searchFilter.subscribe(data => this.searchFilter = data);
+  }
+
+  doesContain(filter: string, data: string) {
+    filter = filter.toUpperCase();
+    data = data.toUpperCase();
+    if (data.includes(filter)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}

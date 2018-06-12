c43cfaf54bd5c742157d223d35760a371fb9ece1
Geri Kona <gerikona@hotmail.com>
2018-06-12 07:52:28 -0400

    Displaying queried places and markers on map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {AgmCoreModule} from '@agm/core';
 
@@ -23,9 +24,11 @@ import {HeaderComponent} from './header/header.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyDJE93xbYm6o5ooJVHQbHWqs5-xEQMeOsw'
+      apiKey: 'AIzaSyDJE93xbYm6o5ooJVHQbHWqs5-xEQMeOsw',
+      libraries: ['places']
     })
 
   ],
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, NgZone, ViewChild, ElementRef} from '@angular/core';
+import {CommsService} from '../comms.service';
+
+import {} from 'googlemaps';
+import {AgmMap} from '@agm/core';
+import {MapsAPILoader} from '@agm/core';
+import {forEach} from '@angular/router/src/utils/collection';
+
 
 @Component({
   selector: 'app-map',
@@ -6,19 +13,59 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  searchFilter = '';
+  locationChoosen = false;
+  mapResults: object;
 
   lat = 43.6532;
   lng = -79.3832;
-  locationChoosen = false;
+  zoom = 13;
+  maxMarkers = 10;
 
-  constructor() {
+  constructor(private comms: CommsService,
+              private mapsAPILoader: MapsAPILoader,
+              private ngZone: NgZone) {
   }
 
   ngOnInit() {
+
+    // set current position
+    //this.setCurrentPosition();
+
+    // load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      let search = new google.maps.places.PlacesService(document.createElement('div'));
+      let toronto = new google.maps.LatLng(43.6532, -79.3832);
+      let request: object = {
+        location: toronto,
+        radius: 500,
+        type: ['restaurant'],
+        keyword: 'pizza'
+      };
+      search.nearbySearch(request, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          console.log(results);
+          this.mapResults = results;
+          // for (let r of results) {
+          //   console.log(r.geometry.location.lat() + ' | ' + r.geometry.location.lng());
+          // }
+        }
+      });
+    });
+  }
+
+  private setCurrentPosition() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+        this.zoom = 12;
+      });
+    }
   }
 
   onChooseLocation(event: any) {
-    //console.log(event);
+    console.log(event);
     this.lat = event.coords.lat;
     this.lng = event.coords.lng;
     this.locationChoosen = true;
